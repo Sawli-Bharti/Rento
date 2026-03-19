197c091a1251c5d6569ee89659c19a00d4c2a92f
Bharti-Sawli <16bharti10@gmail.com>
2026-03-20 00:21:15 +0530

    remove exposed secret key

--- rent-management/src/main/java/com/project/rent_management/config/CloudinaryConfig.java
+++ rent-management/src/main/java/com/project/rent_management/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.project.rent_management.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,20 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dcixflcey");
-        config.put("api_key", "213137425559122");
-        config.put("api_secret", "j995xYERJ4HmnwNteeYX3jQiCck");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
